fb597275571922fd9de5a75c04043990d094cbb5
unknown <aayushkumar1470@gmail.com>
2025-09-15 20:16:15 +0530

    Fix: prevent /uploads/* and /admin.html from redirecting to index.html

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
 app.get("*", (req, res, next) => {
+  if (req.path.startsWith("/uploads")) return next();
   if (req.path === "/admin.html") return next(); // don’t override admin
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
